3270e6d0a66038025c1940beff2306d8daf7d483
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-10-05 08:31:47 -0300

    feat: formulário de login

--- front-end/login-page/src/pages/login/Login.tsx
+++ front-end/login-page/src/pages/login/Login.tsx
@@ -1,7 +1,28 @@
-
+import { useState } from "react"
 
 export const Login = () => {
+    const [email, setEmail] = useState('') // essa string vazia é o que vem renderizado por princípio
+    const [password, setPassword] = useState('')
+
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(password)
+    }
     return (
-        <h1>Teste Login</h1>
+        <div>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type= "password"value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+
+                <button type="button" onClick={handleEntrar}>Entrar</button>
+            </form>
+        </div>
     )
 }
